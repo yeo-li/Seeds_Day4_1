ae28dafdcc1e6a6e1043e8a2821a8ffe4466f686
yeo-li <81564503+yeo-li@users.noreply.github.com>
2025-01-24 16:09:53 +0900

    Seeds 1주 4일차 문제3 코드 추가

--- 문제3.java
+++ 문제3.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class TemperatureConverter {
+public class UserRegistration {
 
   public static void main(String[] args) throws Exception {
 
