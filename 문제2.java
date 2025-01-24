3b9f8be4a79a7fd9e1cc6c48631d2b0b2a18c661
yeo-li <81564503+yeo-li@users.noreply.github.com>
2025-01-24 15:35:20 +0900

    Seeds 1주 4일차 문제2 코드 추가

--- 문제2.java
+++ 문제2.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class TemperatureConverter {
+public class PalindromeChecker {
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
